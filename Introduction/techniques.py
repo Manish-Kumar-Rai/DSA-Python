d9ffc4034ddebc4f9622e90b14f9671658aa886a
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2024-01-20 11:38:10 +0530

    learning comparisons in techniques.py file

--- Introduction/techniques.py
+++ Introduction/techniques.py
@@ -1,12 +1,21 @@
 #------------------- Simple Techniques that are useful --------------------
 from pprint import pprint
 from operator import itemgetter
+from collections import defaultdict, Counter
 #-- Comparisons
 myTuple = (5,7,8,9,10,1,3)
 largest_number = max(((myTuple[i],i) for i, _ in enumerate(myTuple)))
-print(largest_number)
+# print(largest_number)
 
 myList = [("a",1),("b",5),("c",8),("d",2)]
 myList.sort(key=itemgetter(1))
-print(myList)
+# print(myList)
+
+myList = list("Mississippi".lower())
+frequency_dict = defaultdict(int)
+for letter in myList:
+    frequency_dict[letter] += 1
+# pprint(frequency_dict)
+
+# pprint(Counter(myList).most_common(2))
 
