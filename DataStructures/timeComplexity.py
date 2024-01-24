1662877b31a6f8f9f337b652b0f6fc7480144ce5
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2024-01-24 16:02:27 +0530

    Implement queue using linked list

--- DataStructures/timeComplexity.py
+++ DataStructures/timeComplexity.py
@@ -624,3 +624,68 @@ class OurQueue:
 # print(queue.dequeue())
 # print(queue.dequeue())
 # print(queue.dequeue())
+
+
+#Using linked list
+    
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class OurQueue2:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.n = 0
+
+    def __len__(self):
+        return self.n
+    
+    def __str__(self):
+        curr = self.head
+        result = ""
+        while curr != None:
+            result += str(curr.data) + "->"
+            curr = curr.next
+        return result[:-2]
+    
+    def enqueue(self,data):
+        node = Node(data)
+        if self.tail == None:
+            self.tail = node
+            self.head = node
+        else:
+            self.tail.next = node
+            self.tail = node
+        self.n += 1
+
+    def dequeue(self):
+        if self.head == None:
+            return "Empty Queue"
+        item = self.head.data
+        self.head = self.head.next
+        self.n -= 1
+        return item
+    
+    def __getitem__(self,index):
+        if self.head == None:
+            return
+        curr = self.head
+        pos = 0
+        while curr != None:
+            if pos == index:
+                return curr.data
+            curr = curr.next
+            pos += 1
+    
+queue2 = OurQueue2()
+queue2.enqueue("Manish")
+queue2.enqueue("Kumar")
+queue2.enqueue("Rai")
+print(len(queue2))
+print(queue2)
+print(queue2[1])
+# print(queue2.dequeue())
+# print(len(queue2))
+# print(queue2)
